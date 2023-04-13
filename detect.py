2c2b621c0eabd94284f728b4c8b803585532b98f
Bhumika05390 <79781292+Bhumika05390@users.noreply.github.com>
2023-04-13 11:57:05 +0530

    Update detect.py
    Signed-off-by: Bhumika05390 <79781292+Bhumika05390@users.noreply.github.com>

--- detect.py
+++ detect.py
@@ -259,3 +259,18 @@ def main(opt):
 if __name__ == '__main__':
     opt = parse_opt()
     main(opt)
+ def check_imshow(warn=False): 
+     # Check if environment supports image displays 
+     try: 
+         assert not is_jupyter() 
+         assert not is_docker() 
+         cv2.imshow('test', np.zeros((1, 1, 3))) 
+         cv2.waitKey(1) 
+         cv2.destroyAllWindows() 
+         cv2.waitKey(1) 
+         return True 
+     except Exception as e: 
+         if warn: 
+             LOGGER.warning(f'WARNING ⚠️ Environment does not support cv2.imshow() or PIL Image.show()\n{e}') 
+         return False 
+  
